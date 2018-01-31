bfb30cef2d8cc881dc72eea7844b85b002c6b8f2
ZongDuCha <1367243169@qq.com>
2018-01-31 22:10:44 +0800

    magnifier

--- zd-magnifier/js/zd-magnifier.js
+++ zd-magnifier/js/zd-magnifier.js
@@ -2,6 +2,15 @@
     $.fn.zdMagnifier = function(options){
         console.log(1)
         var el = $(this);
-        
+
+        // 缩略图li
+        var _zdLi = el.find('.zd-wrapper--list li')
+        _zdLi.eq(0).addClass('active')
+
+        function getActive(){
+            var _Active = el.find('.zd-wrapper--list .active').index()
+            return _Active;
+        }
+        getActive();
     }
 })(jQuery);
